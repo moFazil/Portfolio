36020bff96cc61b7a07f3e64015526d0155ae5d0
moalifaz <fazelcm2000@gmail.com>
2023-09-26 21:32:01 +0530

    Update

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { HiMenu } from "react-icons/hi";
-import { RxCross2 } from "react-icons/rx";
 
 const navItems = [
   {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import Contact from "../components/contact/Contact";
-import Footer from "../components/footer/Footer";
+import NavBar from "../Components/Navbar/Navbar";
 import Hero from "../Components/Hero/Hero";
-import NavBar from "../components/navbar/NavBar";
-import Resume from "../components/resume/Resume";
-import Skills from "../components/skills/Skills";
-import Work from "../components/work/Work";
+import Skills from "../Components/Skills/Skills";
+import Work from "../Components/Work/Work";
+import Resume from "../Components/Resume/Resume";
+import Contact from "../Components/Contact/Contact";
+import Footer from "../Components/Footer/Footer";
+
 
 const Home = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -14,13 +15,20 @@ const Home = () => {
   }
   return (
     <>
-      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      {/* <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Skills darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Work darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Resume darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> */}
+      <NavBar/>
+      <Hero/>
+      <Skills/>
+      <Work/>
+      <Resume/>
+      <Contact/>
+      <Footer/>
     </>
   );
 };
